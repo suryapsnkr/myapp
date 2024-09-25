9c3a5b349d402a501b8f8f7355df0b10edebd3c6
Surya Prakash Sonkar <91277661+suryapsnkr@users.noreply.github.com>
2024-09-25 23:00:42 +0530

    Add files via upload

---
+++ precache-manifest.44dfd12ec5275f5c0add69e805ff5c3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0adcc65618b45277e9a07a210cfb30a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c18fd48b8e1caaa1ce07",
+    "url": "/static/css/main.d6d57890.chunk.css"
+  },
+  {
+    "revision": "d1cf27ee85e46065c40d",
+    "url": "/static/js/2.69a7c13a.chunk.js"
+  },
+  {
+    "revision": "c716cd336f46221c026f50ff77eb6409",
+    "url": "/static/js/2.69a7c13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d776de3425e90ce8baf8",
+    "url": "/static/js/3.af1fc242.chunk.js"
+  },
+  {
+    "revision": "c18fd48b8e1caaa1ce07",
+    "url": "/static/js/main.4eb34820.chunk.js"
+  },
+  {
+    "revision": "146e8bb4e4804ec8a883",
+    "url": "/static/js/runtime-main.08c142fd.js"
+  }
+]);
