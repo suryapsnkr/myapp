1f00e15a43f5d53ed56ba82d3c5ef3cb1bc9f360
Surya Prakash Sonkar <91277661+suryapsnkr@users.noreply.github.com>
2024-09-29 16:47:35 +0530

    Add files via upload

---
+++ precache-manifest.9b6ddaa47682eb699de6a3d4213a3566.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bc7c6a297a8c7fc031537aac311be1",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "df0df35d1223bd0be6c7",
+    "url": "/myapp/static/css/main.d6d57890.chunk.css"
+  },
+  {
+    "revision": "d1cf27ee85e46065c40d",
+    "url": "/myapp/static/js/2.69a7c13a.chunk.js"
+  },
+  {
+    "revision": "c716cd336f46221c026f50ff77eb6409",
+    "url": "/myapp/static/js/2.69a7c13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d776de3425e90ce8baf8",
+    "url": "/myapp/static/js/3.af1fc242.chunk.js"
+  },
+  {
+    "revision": "df0df35d1223bd0be6c7",
+    "url": "/myapp/static/js/main.c781fac2.chunk.js"
+  },
+  {
+    "revision": "87a748e61a501117f3f4",
+    "url": "/myapp/static/js/runtime-main.96f7c3ae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44dfd12ec5275f5c0add69e805ff5c3a.js"
+  "/myapp/precache-manifest.9b6ddaa47682eb699de6a3d4213a3566.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
